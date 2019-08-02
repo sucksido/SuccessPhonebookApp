4dd5e96f3181b677b25b7b36ee4b0bd806d76aec
sucksido <success.shibambu25@gmail.com>
2019-08-02 23:19:54 +0200

    removed unused code
    removed unused code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { DataService } from '../data.service';
-import { ContactData } from '../contact-data.service';
 
 @Component({
   selector: 'app-home',
