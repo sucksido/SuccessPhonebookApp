9e04c437b19a654d6072428f2bcd6301aee4a6ce
sucksido <success.shibambu25@gmail.com>
2019-08-02 20:11:55 +0200

    initial commit
    initial commit - i worked locally and committed everything in 1 commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { ContactData } from '../contact-data.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html'
+})
+export class HomeComponent implements OnInit {
+ constructor(){}
+ ngOnInit(){}
+
+}
