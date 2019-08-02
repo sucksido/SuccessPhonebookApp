cf8b1f3497951318ba70118c748fca532c71c1e9
sucksido <success.shibambu25@gmail.com>
2019-08-02 23:35:11 +0200

    added styling to the page
    added styling to the page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html'
-})
-export class HomeComponent implements OnInit {
- constructor(){}
- ngOnInit(){}
+  templateUrl: './home.component.html',
+  styles: [`
+    a {
+      color: tomato;
+      display: inline-block;
+      margin: 15px;
+      font-size: 16px;
+      text-transform: capitalize;
 
+    }
+  `]
+})
+
+export class HomeComponent {
+ constructor() {}
 }
